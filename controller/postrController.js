6b46cad02e91b9f2ac6ad963006269809da6c887
AliMaher <ali.maher0013@gmail.com>
2026-03-29 03:24:15 +0200

    fix : fix some bugs

--- controller/postrController.js
+++ controller/postrController.js
@@ -52,13 +52,18 @@ module.exports.getAllPostsctrl = asyncHandler(async (req, res) => {
   const POST_PER_PAGE = 3;
   const { pageNumber, category } = req.query;
   let posts;
+
   if (pageNumber) {
+    const page = Math.max(1, parseInt(pageNumber));
     posts = await Post.find()
-      .skip((pageNumber - 1) * POST_PER_PAGE)
+      .sort({ createdAt: -1 }) // <--- ضيف السطر ده هنا للترتيب من الأحدث للأقدم
+      .skip((page - 1) * POST_PER_PAGE)
       .limit(POST_PER_PAGE)
       .populate("user", ["-password"]);
   } else if (category) {
-    posts = await Post.find({ category }).populate("user", ["-password"]);
+    posts = await Post.find({ category })
+      .sort({ createdAt: -1 }) // <--- وضماناً للترتيب هنا أيضاً
+      .populate("user", ["-password"]);
   } else {
     posts = await Post.find()
       .sort({ createdAt: -1 })
@@ -66,7 +71,6 @@ module.exports.getAllPostsctrl = asyncHandler(async (req, res) => {
   }
   res.status(200).json(posts);
 });
-
 module.exports.getSingelPostctrl = asyncHandler(async (req, res) => {
   const post = await Post.findById(req.params.id)
     .populate("user", ["-password"])
@@ -102,41 +106,9 @@ module.exports.deletePostctrl = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports.getPostCountctrl = asyncHandler(async (req, res) => {
-  const count = await Post.countDocuments();
-  res.status(200).json(count);
-});
-
 // update posts
 // api/posts/:id
 
-module.exports.updatePostImageCtrl = asyncHandler(async (req, res) => {
-  const { error } = validateUpdatePost(req.body);
-  if (error) {
-    return res.status(400).json({ message: error.details[0].message });
-  }
-  const post = await Post.findById(req.params.id);
-  if (!post) {
-    return res.status(404).json({ message: "Post not found" });
-  }
-  if (req.user.id !== post.user.toString()) {
-    return res.status(403).json({
-      message: "Access Denied, you are not allowed to update this post",
-    });
-  }
-  const updatedPost = await Post.findByIdAndUpdate(
-    req.params.id,
-    {
-      $set: {
-        title: req.body.title,
-        description: req.body.description,
-        category: req.body.category,
-      },
-    },
-    { new: true },
-  ).populate("user", ["-password"]);
-  return res.status(200).json(updatedPost);
-});
 // update post photo
 module.exports.updatePostImageCtrl = asyncHandler(async (req, res) => {
   if (!req.file) {
