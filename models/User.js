9a134927d491d24e53b898d8cc307824a63d1b67
AliMaher <ali.maher0013@gmail.com>
2026-04-03 17:16:28 +0200

    update user photo validation

--- models/User.js
+++ models/User.js
@@ -28,8 +28,7 @@ const UserSchema = new mongoose.Schema(
     profilePhoto: {
       url: {
         type: String,
-        default:
-          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+        default: null,
       },
       publicId: {
         type: String,
